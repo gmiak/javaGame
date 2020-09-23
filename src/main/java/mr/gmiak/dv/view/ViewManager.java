45cac986a2c37bfb83982032d108db94fd5d2f1f
Georges Kayembe <kayss.g@gmail.com>
2020-09-23 12:57:55 +0200

    Added menu, logo & background-image

--- src/main/java/mr/gmiak/dv/view/ViewManager.java
+++ src/main/java/mr/gmiak/dv/view/ViewManager.java
@@ -3,13 +3,17 @@ package mr.gmiak.dv.view;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ViewManager {
-    //private AnchorPane mainPane;
+    private AnchorPane mainPane;
     private Scene mainScene;
     private Stage mainStage;
     private static final int WIDTH = 1000;
@@ -17,11 +21,20 @@ public class ViewManager {
     //constructor
     public ViewManager() throws IOException {
         //mainPane = new AnchorPane();
-        Parent mainPane = FXMLLoader.load(getClass().getClassLoader().getResource("startMenu.fxml"));
+        mainPane = FXMLLoader.load(getClass().getClassLoader().getResource("startMenu.fxml"));
         mainScene = new Scene(mainPane, WIDTH, HEIGHT);
         mainStage = new Stage();
         mainStage.setScene(mainScene);
+        createBackground();
     }
     //return the main stage
     public Stage getMainStage() {return mainStage;}
+    //add background image
+    private void createBackground() throws IOException {
+        InputStream inStream = Files.newInputStream(Paths.get("src/main/resources/bc1.jpg"));
+        Image backgroundImage = new Image(inStream, 1000, 700, false, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(background));
+
+    }
 }
