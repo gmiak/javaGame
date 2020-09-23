fe2d76d0d3fc2c39a214fa1b1bc04e599e337a68
Georges Kayembe <kayss.g@gmail.com>
2020-09-23 13:43:46 +0200

    Added subscene onclick play

--- src/main/java/mr/gmiak/dv/controller/StartMenu.java
+++ src/main/java/mr/gmiak/dv/controller/StartMenu.java
@@ -1,4 +1,40 @@
 package mr.gmiak.dv.controller;
 
+import javafx.animation.TranslateTransition;
+import javafx.scene.SubScene;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
 public class StartMenu {
+    public SubScene subSceneMenu;
+    private boolean isHidden;
+    //constructor
+    public StartMenu() {
+        isHidden=true;
+    }
+    //on click exit
+    public void onClickedExit(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
+
+    public void onClickedPlay(MouseEvent mouseEvent) {
+        moveSubscene();
+    }
+
+    public void onClickedOptions(MouseEvent mouseEvent) {
+    }
+    //mouve subscene
+    public void moveSubscene() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(0.3));
+        transition.setNode(subSceneMenu);
+        if (isHidden) {
+            transition.setToX(-650);
+            isHidden = false;
+        } else {
+            transition.setToX(0);
+            isHidden = true;
+        }
+        transition.play();
+    }
 }
