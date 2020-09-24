df64b55adec69696398aece37fcd14d50f11e431
Georges Kayembe <kayss.g@gmail.com>
2020-09-24 08:30:36 +0200

    Added menu -choose your game-

--- src/main/java/mr/gmiak/dv/controller/StartMenu.java
+++ src/main/java/mr/gmiak/dv/controller/StartMenu.java
@@ -2,15 +2,28 @@ package mr.gmiak.dv.controller;
 
 import javafx.animation.TranslateTransition;
 import javafx.scene.SubScene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.util.Duration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class StartMenu {
-    public SubScene subSceneMenu;
+    public AnchorPane subSceneMenu;
+    public AnchorPane subSceneMenu1;
+    public ImageView breakout;
+    public ImageView spacerunner;
     private boolean isHidden;
+    private boolean isHidden1;
     //constructor
-    public StartMenu() {
+    public StartMenu() throws IOException {
         isHidden=true;
+        isHidden1=true;
     }
     //on click exit
     public void onClickedExit(MouseEvent mouseEvent) {
@@ -22,8 +35,9 @@ public class StartMenu {
     }
 
     public void onClickedOptions(MouseEvent mouseEvent) {
+        moveSubscene1();
     }
-    //mouve subscene
+    //mouve subscene-menu
     public void moveSubscene() {
         TranslateTransition transition = new TranslateTransition();
         transition.setDuration(Duration.seconds(0.3));
@@ -36,5 +50,25 @@ public class StartMenu {
             isHidden = true;
         }
         transition.play();
+    }
+    //mouve subscene-menu1
+    public void moveSubscene1() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(0.3));
+        transition.setNode(subSceneMenu1);
+        if (isHidden1) {
+            transition.setToY(-500);
+            isHidden1 = false;
+        } else {
+            transition.setToY(0);
+            isHidden1 = true;
+        }
+        transition.play();
+    }
+
+    public void onMouseClickedGame(MouseEvent mouseEvent) {
+    }
+
+    public void onMouseReleasedGame(MouseEvent mouseEvent) {
     }
 }
